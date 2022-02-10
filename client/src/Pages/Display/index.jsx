3a126853a77688a17dbf7d9c6eeaea1d92a34237
Sheikh Salman <sheikhsalman2013@gmail.com>
2022-02-11 00:01:24 +0600

    acc

--- client/src/Pages/Display/index.jsx
+++ client/src/Pages/Display/index.jsx
@@ -157,7 +157,9 @@ const Display = () => {
             transform:
               recievedData.type == "custom"
                 ? `rotate(${recievedData.angle}deg)`
-                : `rotateX(${recievedData.rotationState?.roll}deg)rotateY(${recievedData.rotationState?.pitch}deg)rotateZ(${recievedData.rotationState?.yaw}deg)`,
+                : `rotateX(${0}deg)rotateY(${0}deg)rotateZ(${
+                    recievedData.rotationState?.yaw
+                  }deg)`,
             transition: "unset",
           }}
         />
